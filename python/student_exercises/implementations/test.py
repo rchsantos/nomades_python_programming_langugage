2e544dd6da413dd35666e0bbc2ec66a57f22b011
Antonio Pisanello <antonio@apisanello.dev>
2025-05-22 17:35:42 +0200

    Add titanic exercise

--- python/student_exercises/implementations/test.py
+++ python/student_exercises/implementations/test.py
@@ -1,9 +1,17 @@
-def square(x):
-    return x**2
+import numpy as np
+import matplotlib.pyplot as plt
 
-def main():
-    x = int(input("Enter a number: "))
-    print(square(x))
 
-if __name__ == '__main__':
-    main()
+ax = plt.subplot(projection='3d')
+
+# Prepare arrays x, y, z
+theta = np.linspace(-4 * np.pi, 4 * np.pi, 100)
+z = np.linspace(-2, 2, 100)
+r = z**2 + 1
+x = r * np.sin(theta)
+y = r * np.cos(theta)
+
+ax.plot(x, y, z, label='parametric curve')
+ax.legend()
+
+plt.show()
